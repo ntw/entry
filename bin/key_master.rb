866bcfbb29ca4eebda8c2cada743d416f7e00249
Nicholas Weigand <dawtsf@gmail.com>
2013-04-29 14:48:45 -0700

    added dynamic options to key_master

--- bin/key_master.rb
+++ bin/key_master.rb
@@ -1,3 +1,13 @@
+#!/usr/bin/env ruby
+
 require '../lib/entry.rb'
+require 'trollop'
 
-
+opts = Trollop::options do
+	opt :add, "Add a user to the database", :short => 'a'
+	opt :delete, "Delete a user from the database", :short => 'd'
+	opt :update, "Update a user in the database", :short => 'u'
+	Entry::User.attributes.each do |a|
+		opt a.to_sym, "The user's #{a.to_s}"
+	end
+end
