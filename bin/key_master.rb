9860de313c1780a0be164a7f50bca600c797b457
Nicholas Weigand <dawtsf@gmail.com>
2013-04-29 16:37:30 -0700

    mostly functioning library

--- bin/key_master.rb
+++ bin/key_master.rb
@@ -3,18 +3,25 @@
 require '../lib/entry.rb'
 require 'trollop'
 
-SUB_COMMANDS = %w(add delete update)
+SUB_COMMANDS = %w(add delete update authorize)
 Trollop::options do
+	stop_on SUB_COMMANDS
 end
 cmd = ARGV.shift
 opts = case cmd
-when "add"
+when "add", "update"
 	Trollop::options do
 		Entry::User.attributes.each do |a|
 			opt a.to_sym, "The user's #{a.to_s}", :type => String
 		end
 	end
-when "delete", "update"
+when "delete"
+	Trollop::options do
+		Entry::User.indices.each do |i|
+			opt i.to_sym, "The user's #{i.to_s}", :type => String
+		end
+	end
+when "authorize"
 	Trollop::options do
 		Entry::User.indices.each do |i|
 			opt i.to_sym, "The user's #{i.to_s}", :type => String
